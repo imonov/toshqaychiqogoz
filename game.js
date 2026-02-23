05e6c26085f88c6d47f709a8212f9f794648dc28
Anvar <anvar@imonov.uz>
2026-02-23 14:35:02 +0500

    local storagega yozish qo'shildi

--- game.js
+++ game.js
@@ -6,8 +6,17 @@ const values = {
     2: "Qog'oz",
 };
 
+let winCnt =
+    localStorage.getItem("win") == null ? 0 : localStorage.getItem("win");
+let rscCnt =
+    localStorage.getItem("rsc") == null ? 0 : localStorage.getItem("rsc");
+let eqlCnt =
+    localStorage.getItem("eql") == null ? 0 : localStorage.getItem("eql");
+
 const selectBtn = document.querySelectorAll(".btn-select");
 const resultText = document.querySelector(".result");
+const countLbl = document.querySelector(".count");
+countLbl.innerHTML = `Yutishlar: ${winCnt}; Mag'lubiyatlar: ${rscCnt}; Durranglar: ${eqlCnt}`;
 
 const getComVal = () => Math.floor(Math.random() * 3);
 
@@ -15,12 +24,20 @@ let result = false;
 
 selectBtn.forEach((btn) => {
     btn.addEventListener("click", function (e) {
+        if (winCnt + eqlCnt + rscCnt >= 10) {
+            winCnt = 0;
+            eqlCnt = 0;
+            rscCnt = 0;
+            return;
+        }
+
         const sVal = this.dataset.value;
         const compValue = getComVal();
 
         if (sVal == compValue) {
             console.log("Durang");
             result = "durrang";
+            eqlCnt++;
         } else if (
             (sVal == 0 && compValue == 1) ||
             (sVal == 1 && compValue == 2) ||
@@ -28,9 +45,11 @@ selectBtn.forEach((btn) => {
         ) {
             console.log(`Yutdik ${values[sVal]}: ${values[compValue]}`);
             result = true;
+            winCnt++;
         } else {
             console.log(`Yutqazdik ${values[sVal]}: ${values[compValue]}`);
             result = false;
+            rscCnt++;
         }
 
         resultText.innerHTML = `
@@ -48,5 +67,10 @@ selectBtn.forEach((btn) => {
         }
 
         resultText.innerHTML += resHTML;
+        countLbl.innerHTML = `Yutishlar: ${winCnt}; Mag'lubiyatlar: ${rscCnt}; Durranglar: ${eqlCnt}`;
+
+        localStorage.setItem("win", winCnt);
+        localStorage.setItem("rsc", rscCnt);
+        localStorage.setItem("eql", eqlCnt);
     });
 });
