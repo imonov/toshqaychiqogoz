d70fc0a4fe72945dadc8e73136be662f1ca51918
Anvar <anvar@imonov.uz>
2026-02-23 14:19:15 +0500

    tugadi

---
+++ game.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const values = {
+    0: "Tosh",
+    1: "Qaychi",
+    2: "Qog'oz",
+};
+
+const selectBtn = document.querySelectorAll(".btn-select");
+const resultText = document.querySelector(".result");
+
+const getComVal = () => Math.floor(Math.random() * 3);
+
+let result = false;
+
+selectBtn.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+        const sVal = this.dataset.value;
+        const compValue = getComVal();
+
+        if (sVal == compValue) {
+            console.log("Durang");
+            result = "durrang";
+        } else if (
+            (sVal == 0 && compValue == 1) ||
+            (sVal == 1 && compValue == 2) ||
+            (sVal == 2 && compValue == 0)
+        ) {
+            console.log(`Yutdik ${values[sVal]}: ${values[compValue]}`);
+            result = true;
+        } else {
+            console.log(`Yutqazdik ${values[sVal]}: ${values[compValue]}`);
+            result = false;
+        }
+
+        resultText.innerHTML = `
+<p>Men: ${values[sVal]}<br>Kompyuter: ${values[compValue]}</p>
+`;
+
+        let resHTML = "";
+
+        if (result === true) {
+            resHTML = `<h2 class="win">Yutdingiz 🎉</h2>`;
+        } else if (result === false) {
+            resHTML = `<h2 class="lose">Yutqazdingiz 😢</h2>`;
+        } else {
+            resHTML = `<h2 class="draw">Durang 🤝</h2>`;
+        }
+
+        resultText.innerHTML += resHTML;
+    });
+});
